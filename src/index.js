6ef1cfbeedc078f52a33115757534cf9b361be97
LaubetBeltran <laubetpop@gmail.com>
2020-12-03 19:06:28 -0600

    organizacion de codigo in inexjs y comentarios

--- src/index.js
+++ src/index.js
@@ -3,15 +3,14 @@
 //console.log(cipher);
 
 ///////////////////////////////////////7 aqui se ivoca toda la funcionalidad que interactua con el dom
-
+// Botón de la página de inicio que lleva a la pantalla input.
 function comenzar(){
     location.href= ("#pantallaInput");
     document.getElementById("pantallaInput").style.display='block';
     document.getElementById('pantallaInicio').style.display='none';
   }
   
-  
-  
+ 
 // PARA CIFRAR UN MENSAJE
 function obtenerMensajeCifrado(){             
   var texto = document.getElementById('mensaje').value;
@@ -26,7 +25,6 @@ function obtenerMensajeCifrado(){
           var cifrado = String.fromCharCode(numFinal);
           var text = document.createTextNode(cifrado); 
           document.getElementById('MensajeCifrado').appendChild(text);
-
          // document.write(cifrado);  
           } else if(num == 32){
             var espacio = document.createTextNode(" "); 
@@ -50,14 +48,14 @@ function obtenerMensajeCifrado(){
           } else {
             var simboloDesconocido = String.fromCharCode(num);
             var textoDesconicido = document.createTextNode(simboloDesconocido);
-          document.getElementById('MensajeCifrado').appendChild(textoDesconicido);
+            document.getElementById('MensajeCifrado').appendChild(textoDesconicido);
             //document.write(simboloDesconocido);
           }
           }
 
 
 
-    //DOM PARA OCULTAR INPUT E IR AL OUPUT
+    //DOM PARA OCULTAR INPUT E IR AL OUPUT CON EL MENSAJE CIFRADO
     location.href = '#outputCifrarMensaje'
     document.getElementById("outputCifrarMensaje").style.display = 'block';
     document.getElementById("pantallaInput").style.display = 'none';
@@ -75,38 +73,58 @@ function obtenerMensajeCifrado(){
     
     document.getElementById('MensajeDescifrado').innerHTML= mensaje + "   (Número de cifrado:" + numeroDeCifrado + ").";
      
-    //alert("Tu mensaje con " + numeroDeCifrado + " desplazamiento(s) de letra(s) está listo: " + mensaje);
-  
+    
+    //DOM PARA OCULTAR INPUT E IR AL OUPUT CON EL MENSAJE DESCIFRADO
     location.href = '#outputDescifrarMensaje'
     document.getElementById("outputDescifrarMensaje").style.display = 'block';
     document.getElementById("pantallaInput").style.display = 'none';
   }
   
   
-  //BOTONES PARA VOLVER ESCRIBIR UN MENSAJE NUEVO
+//BOTONES PARA VOLVER ESCRIBIR UN MENSAJE NUEVO
+
+  //Botón para  volver al input y limparlo desde la Pantalla outputCifrarMensaje
   function nuevoMensaje1()
   {
     location.href = "#pantallaInput";
     document.getElementById("pantallaInput").style.display= 'block';
     document.getElementById("outputCifrarMensaje").style.display= 'none';
     document.getElementById("mensaje").value = "";
-    document.getElementById("numeroDeCifrado").value = "";
-  
-  
+    document.getElementById("numeroDeCifrado").value = "0";
   }
-  
+
+  // Botón para volver al input y limpiarlo desde la Pantalla outputDescifrarMensaje
   function nuevoMensaje2()
   {
     location.href = "#pantallaInput";
     document.getElementById("pantallaInput").style.display= 'block';
     document.getElementById("outputDescifrarMensaje").style.display= 'none';
     document.getElementById("mensaje").value = "";
-    document.getElementById("numeroDeCifrado").value = "";
+    document.getElementById("numeroDeCifrado").value = "0";
   
   }
-  
-  
-  
+  // Botón para  volver al input desde la Pantalla navegar1
+  function nuevoMensaje3(){
+    location.href = "#pantallaInput";
+    document.getElementById("navegar1").style.display= 'none'
+    document.getElementById("pantallaInput").style.display= 'block';
+  }
+  // Botón para  volver al input desde la Pantalla navegar2
+  function nuevoMensaje4(){
+    location.href="#pantallaInput";
+    document.getElementById("navegar2").style.display= 'none'
+    document.getElementById("pantallaInput").style.display= 'block';
+  }
+  // Botón para  volver al input desde la Pantalla navegar3
+  function nuevoMensaje5(){
+    location.href="#pantallaInput";
+    document.getElementById("navegar3").style.display= 'none'
+    document.getElementById("pantallaInput").style.display= 'block';
+  }
+
+//MOSTRAR/OCULTAR DE LOS ENLACES DE LA BARRA DE NAVEACIÓN
+
+  //Para mostrar Pantalla navegación 1 ¿qué es el cifrado César?
   function mostrarNavegar1(){
     location.href = "#navegar1";
     document.getElementById("navegar1").style.display= 'block';
@@ -118,14 +136,7 @@ function obtenerMensajeCifrado(){
     document.getElementById('pantallaInicio').style.display='none';
   
   }
-  
-  function nuevoMensaje3(){
-    location.href = "#pantallaInput";
-    document.getElementById("navegar1").style.display= 'none'
-    document.getElementById("pantallaInput").style.display= 'block';
-  }
-  
-  
+  //Para mostrar Pantalla navegación 2 ¿qué utilidad tiene?
   function mostrarNavegar2(){
     location.href="#navegar2";
     document.getElementById("navegar2").style.display= 'block'
@@ -137,15 +148,8 @@ function obtenerMensajeCifrado(){
     document.getElementById('pantallaInicio').style.display='none';
   
   }
-  
-  function nuevoMensaje4(){
-    location.href="#pantallaInput";
-    document.getElementById("navegar2").style.display= 'none'
-    document.getElementById("pantallaInput").style.display= 'block';
-  }
-  
-  
-  
+
+  //Para mostrar Pantalla navegación 3 ¿como cifrar o descifrar un mensaje?
   function mostrarNavegar3(){
     location.href="#navegar3";
     document.getElementById("navegar3").style.display= 'block'
@@ -158,13 +162,10 @@ function obtenerMensajeCifrado(){
   
   }
   
-  function nuevoMensaje5(){
-    location.href="#pantallaInput";
-    document.getElementById("navegar3").style.display= 'none'
-    document.getElementById("pantallaInput").style.display= 'block';
-  }
-  
-  
+
+
+//BOTONES PARA VOLVER AL INICIO
+  //Botón para volver al inicio esde las Pantallas Output y borrar información del input
   function irAInicioClean(){
   
     location.href="#pantallaInicio";
@@ -176,8 +177,10 @@ function obtenerMensajeCifrado(){
     document.getElementById("outputCifrarMensaje").style.display= 'none';
     document.getElementById("pantallaInput").style.display= 'none';
     document.getElementById("mensaje").value = "";
-    document.getElementById("numeroDeCifrado").value = "";
+    document.getElementById("numeroDeCifrado").value = "0";
   }
+
+  // Botón para volver al inicio desde las patallas de la barra de navegación
   function irAInicio(){
   
     location.href="#pantallaInicio";
@@ -189,7 +192,8 @@ function obtenerMensajeCifrado(){
     document.getElementById("outputCifrarMensaje").style.display= 'none';
     document.getElementById("pantallaInput").style.display= 'none';
   }
-  
+
+ //Boton del logo y nombre de la página para refrescar la url 
   function refrescarUrl(){
       location.reload()
   }
