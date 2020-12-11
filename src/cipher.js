185ee52f6c3be3e435c9ce056471297babc7c003
LaubetBeltran <laubetpop@gmail.com>
2020-12-10 20:16:22 -0600

    correcion test de variables en cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,8 @@
 const cipher = {
-  encode : function(texto, offset) {
+  encode : function(offset, string) {
     let mensajeCifrado = "";               
     for(var i = 0; i <= 1000; i++) {
-      var num = texto.charCodeAt(0 +i);
+      var num = string.charCodeAt(0 +i);
       if(num >=65 && num <=90) {
         var resta = num - 65;
         var suma = resta + offset;
@@ -38,35 +38,44 @@ const cipher = {
 
     return mensajeCifrado;
   },
-  decode: function(texto, offset) {
+  decode: function(offset, string) {
   let mensajeDescifrado = "";
   for(var i = 0; i <= 1000; i++) {
-    var num = texto.charCodeAt(0 +i);
+    var num = string.charCodeAt(0 +i);
     if(num >=65 && num <=90) {
       var resta = num - 65;
       var residuo = offset % 26;
       if (residuo <= resta){
-        var resta2 = resta - residuo;
+        var resta2d1 = resta - residuo;
+        var numFinal1 = resta2d1 + 65;
+        var descifrado1 = String.fromCharCode(numFinal1);
+        mensajeDescifrado= mensajeDescifrado.concat(descifrado1);
+
       } else if (residuo > resta){
         var diferencia = residuo - resta;
-            var resta2 = 26 - diferencia;
+        var resta2d2 = 26 - diferencia;
+        var numFinal2 = resta2d2 + 65;
+        var descifrado2 = String.fromCharCode(numFinal2);
+        mensajeDescifrado= mensajeDescifrado.concat(descifrado2);
       }
-      var numFinal = resta2 + 65;
-      var descifrado = String.fromCharCode(numFinal);
-      mensajeDescifrado= mensajeDescifrado.concat(descifrado);
+
           
     } else if(num >=97 && num <=122) {
-      var resta = num - 97;
-      var residuo = offset % 26;
-      if (residuo <= resta){
-        var resta2 = resta - residuo;
-      } else if (residuo > resta){
-        var diferencia = residuo - resta;
-        var resta2 = 26 - diferencia;
+      var restaMinuscula = num - 97;
+      var residuoMinuscula = offset % 26;
+        if (residuoMinuscula <= restaMinuscula){
+        var resta2Minuscula1 = restaMinuscula - residuoMinuscula;
+        var numFinalMinuscula1 = resta2Minuscula1 + 97;
+        var descifradoMinuscula1 = String.fromCharCode(numFinalMinuscula1);
+        mensajeDescifrado= mensajeDescifrado.concat(descifradoMinuscula1);
+      } else if (residuoMinuscula > restaMinuscula){
+        var diferenciaMinuscula = residuoMinuscula - restaMinuscula;
+        var resta2Minuscula2 = 26 - diferenciaMinuscula;
+        var numFinalMinuscula2 = resta2Minuscula2 + 97;
+        var descifradoMinuscula2 = String.fromCharCode(numFinalMinuscula2);
+        mensajeDescifrado= mensajeDescifrado.concat(descifradoMinuscula2);
       }
-      var numFinal = resta2 + 97;
-      var descifrado = String.fromCharCode(numFinal);
-      mensajeDescifrado= mensajeDescifrado.concat(descifrado);
+
     } else{
       var simboloDesconocido = String.fromCharCode(num);
       mensajeDescifrado = mensajeDescifrado.concat(simboloDesconocido); 
